5ca80ecd6d1749a6a483e49864cd2106708417ed
renato Ferrara <reny.2000@hotmail.it>
2025-06-29 12:33:42 +0200

    [NEW] project creation

---
+++ TrueExercise/TrueExercise/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TrueExercise
+//
+//  Created by Renato Ferrara on 29/06/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TrueExercise/TrueExercise/TrueExerciseApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrueExerciseApp.swift
+//  TrueExercise
+//
+//  Created by Renato Ferrara on 29/06/25.
+//
+
+import SwiftUI
+
+@main
+struct TrueExerciseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
